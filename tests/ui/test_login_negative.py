fb162388877bc402b7adc87b705c63df896ec558
AlexKomanov <akomanov88@gmail.com>
2025-07-07 20:39:25 +0300

    adding attachments to Allure

--- tests/ui/test_login_negative.py
+++ tests/ui/test_login_negative.py
@@ -1,8 +1,9 @@
 from playwright.sync_api import Page, expect
 from pages.login_page import LoginPage
+import allure
 
 
-def test_example_1(login_page) -> None:
+def test_example_1(login_page, page) -> None:
     login_page.navigate()
     login_page.expect_login_credentials_visible()
 
@@ -11,6 +12,7 @@ def test_example_1(login_page) -> None:
     login_page.click_login_button()
 
     login_page.expect_error_message("Epic sadface: Sorry, this user has been locked out.")
+    attach_screenshot(page, "Screenshot test_exampe_1")
 
 
 
@@ -22,3 +24,13 @@ def test_example_2(page: Page, login_page_with_login) -> None:
     page.locator("[data-test=\"password\"]").fill("secret_sauce")
     page.locator("[data-test=\"login-button\"]").click()
     expect(page.locator("[data-test=\"error\"]")).to_contain_text("Epic sadface: Username and password do not match any user in this service")
+    attach_screenshot(page, "Screenshot test_exampe_2")
+
+
+def attach_screenshot(page, name):
+    screenshot = page.screenshot()
+    allure.attach(
+        screenshot,
+        name=name,
+        attachment_type=allure.attachment_type.PNG,
+    )
